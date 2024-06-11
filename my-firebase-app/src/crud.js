9060eb4423efeef3d65af08c375e4ec30a08eb7b
kirkmatic <kirkmatic1@gmail.com>
2024-06-11 12:53:30 +0800

    updated crud.js

--- my-firebase-app/src/crud.js
+++ my-firebase-app/src/crud.js
@@ -1,18 +1,71 @@
-import React from 'react'
-import { db } from './firebase'
-
+import React, { useState } from 'react';
+import { db } from './firebase';
+import { collection, addDoc } from 'firebase/firestore';
 
 const Crud = () => {
+  const [name, setName] = useState('');
+  const [address, setAddress] = useState('');
+  const [email, setEmail] = useState('');
+
+  // Correct way to get a reference to the Firestore collection
+  const dbRef = collection(db, 'my-firebase-app');
+
+  // Function to add data to the Firestore collection
+  const add = async () => {
+    try {
+      const docRef = await addDoc(dbRef, {
+        Name: name,
+        Address: address,
+        Email: email,
+      });
+      alert('Data Added Successfully');
+      console.log('Document written with ID: ', docRef.id);
+    } catch (error) {
+      console.error('Error adding document: ', error);
+      alert('Error adding document: ' + error.message);
+    }
+  };
+
   return (
     <>
-      <div className='form_container'>
-        <h2> Add / Update Form</h2>
-          <div className='box'>
-              <input type='text' placeholder='Fullname' autoComplete='off'></input>
+      <div className="form_container">
+        <h2>Add / Update Form</h2>
+        <div className="box">
+          <input
+            type="text"
+            placeholder="Fullname"
+            autoComplete="off"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Address"
+            autoComplete="off"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            autoComplete="off"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <button onClick={add}>Add</button>
+          <button>Update</button>
+        </div>
+      </div>
+      <div>
+        <h2>CRUD Database</h2>
+        <div className="container">
+          <div className="box">
+            {/* Add your display logic here */}
           </div>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Crud
+export default Crud;
--- my-firebase-app/src/firebase.js
+++ my-firebase-app/src/firebase.js
@@ -1,14 +1,17 @@
-import firebase from 'firebase/compat/app'
-import 'firebase/compat/firestore'
+// src/firebase.js
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCdqykTuQXy-nKnNd0u_YND89Z8BUArogE",
-    authDomain: "my-firebase-app-9b7bc.firebaseapp.com",
-    projectId: "my-firebase-app-9b7bc",
-    storageBucket: "my-firebase-app-9b7bc.appspot.com",
-    messagingSenderId: "894447674368",
-    appId: "1:894447674368:web:abc05ccdf63124393de4e5"
-  };
+  apiKey: "AIzaSyCdqykTuQXy-nKnNd0u_YND89Z8BUArogE",
+  authDomain: "my-firebase-app-9b7bc.firebaseapp.com",
+  projectId: "my-firebase-app-9b7bc",
+  storageBucket: "my-firebase-app-9b7bc.appspot.com",
+  messagingSenderId: "894447674368",
+  appId: "1:894447674368:web:abc05ccdf63124393de4e5"
+};
 
-  const app = firebase.initializeApp(firebaseConfig)
-  export const db = firebase.firestore()
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+export { db };
