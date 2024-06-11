f6259a03c4d894fc04f4912f35319867eb63eba8
kirkmatic <kirkmatic1@gmail.com>
2024-06-11 11:58:11 +0800

    added project

---
+++ my-firebase-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Crud from './crud'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Yow G!!!!</h1>
+      <Crud />
+    </div>
+  )
+}
+
+export default App
---
+++ my-firebase-app/src/crud.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { db } from './firebase'
+
+
+const Crud = () => {
+  return (
+    <>
+      <div className='form_container'>
+        <h2> Add / Update Form</h2>
+          <div className='box'>
+              <input type='text' placeholder='Fullname'></input>
+          </div>
+      </div>
+    </>
+  )
+}
+
+export default Crud
