80e4b82f4a15d5cd6ec7a0c9ac99979c862c0b8c
kirkmatic <kirkmatic1@gmail.com>
2024-06-11 12:08:52 +0800

    edited crud

--- my-firebase-app/src/crud.js
+++ my-firebase-app/src/crud.js
@@ -8,7 +8,7 @@ const Crud = () => {
       <div className='form_container'>
         <h2> Add / Update Form</h2>
           <div className='box'>
-              <input type='text' placeholder='Fullname'></input>
+              <input type='text' placeholder='Fullname' autoComplete='off'></input>
           </div>
       </div>
     </>
