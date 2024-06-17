52e1dbb36eda3f7aeecca05a2e9551a403512a41
Kirk J-Son Matic <kirkmatic1@gmail.com>
2024-06-17 17:43:30 +0800

    change bg of homepage

--- my-firebase-app/src/components/HomeSection.js
+++ my-firebase-app/src/components/HomeSection.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const HomeSection = () => {
   return (
-    <section className="bg-home-section w-auto h-96 bg-cover bg-center px-1 py-16">
+    <section className="bg-home-section w-auto min-h-px bg-cover bg-center px-1 py-16">
       <div className="max-w-2xl ml-28 text-left">
         <h1 className="text-red-100 text-5xl font-extrabold mb-6">Create a lasting memory that cherished forever</h1>
         <p className="text-red-100 text-lg font-medium mb-6">Our experienced artists specialize in creating stunning lifelike portraits that capture the true essence of their beauty</p>
-        <button className='bg-red-400 font-bold rounded-xl text-red-100 h-10 w-auto text-2xl text-left px-3 py-1'>Message Us</button>
+        <button className='bg-red-400 font-bold rounded-xl text-red-100 h-10 w-auto text-xl text-left px-3 py-1'>Message Us</button>
       </div>
     </section>
   );
